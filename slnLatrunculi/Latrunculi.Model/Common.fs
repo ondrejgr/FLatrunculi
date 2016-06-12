d646cbf133c4f284a179e3793eb2b4bab098822e
ondrejgr <ondrej.gratz@ONDRAS-DEV>
2016-06-12 21:18:44 +0200

    add ReturnFrom to MaybeBuilder

--- slnLatrunculi/Latrunculi.Model/Common.fs
+++ slnLatrunculi/Latrunculi.Model/Common.fs
@@ -5,18 +5,26 @@ module Common
         | Success of 'TSuccess 
         | Error of 'TError
 
+    let tryChangeError m e =
+        match m with
+        | Success x -> Success x
+        | Error _ -> Error e
+
     let getObjExn c =
         match c with
         | Success c -> c
         | _ -> failwith "Unable to extract object instance from function result, because called function has failed."
 
     type MaybeBuilder() =
-        member this.Bind(x, f) = 
-            match x with
+        member this.Bind(m, f) = 
+            match m with
             | Success s -> f s
             | Error e -> Error e
 
         member this.Return(x) = 
-            x
+            Success x
+
+        member this.ReturnFrom(m) =
+            m
 
     let maybe = new MaybeBuilder()
